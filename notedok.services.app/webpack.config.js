9c47e45b15cfdad8e2ed23e97cc483fa98fc5cd1
artem_kondratyev <artemkv@gmail.com>
2018-11-17 18:46:54 +0100

    npm clean script

--- notedok.services.app/webpack.config.js
+++ notedok.services.app/webpack.config.js
@@ -1,7 +1,12 @@
+const path = require('path');
 const htmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/notedok.js',
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: './bundle.js'
+    },
     module: {
         rules: [
             {
